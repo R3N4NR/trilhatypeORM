faa37c7400454a5fc64bdbf4f73badace888ff36
R3N4NR <renanramalhou@gmail.com>
2022-07-25 19:53:05 -0300

    🎉 configura o projeto
    configuracoes iniciais ajustadas

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    client: {
+  
+      service: "my-apollo-service", // the name of your graph in Studio
+  
+    },
+  
+  };
---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppService } from './app.service';
+import { StudentsModule } from './module/students/students.module';
+
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      installSubscriptionHandlers: true,
+      
+      
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'admin',
+      database: 'test',
+      entities: ["entity/*.js,modules/**/entity/*.js "],
+      synchronize: true,
+      logging:true
+    }),
+    StudentsModule
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
+    
